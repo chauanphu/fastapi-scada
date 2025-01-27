5ecb4eb69f34eb60db25ff25ce3abe6863bde89b
Phu Chau <chauanphu@gmail.com>
2025-01-27 14:09:46 +0700

    bug: redundant DATABASE_URL

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,9 +1,6 @@
 from enum import Enum
 from decouple import config
 # Import all the models in the database
-
-URL_DATABASE = config("DATABASE_URL")
-
 REDIS_HOST = config("REDIS_HOST")
 REDIS_PORT = config("REDIS_PORT")
 
